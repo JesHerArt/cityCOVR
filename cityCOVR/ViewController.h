8cdc82e622eebde7a838e3471f4c89f513e7ac39
Jessica Hernandez <jesherart@fullsail.edu>
2016-03-05 20:40:37 -0500

    Initial Commit

---
+++ cityCOVR/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  cityCOVR
+//
+//  Created by Jessica Hernandez on 3/5/16.
+//  Copyright © 2016 Mango Hackers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ cityCOVR/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  cityCOVR
+//
+//  Created by Jessica Hernandez on 3/5/16.
+//  Copyright © 2016 Mango Hackers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
